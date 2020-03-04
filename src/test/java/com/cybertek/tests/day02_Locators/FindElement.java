e27e3781c5867f6400b9fd91af52c802e9be39a3
HCavus <halimeecavus@gmail.com>
2020-03-03 20:22:12 -0500

    commit 2'

--- src/test/java/com/cybertek/tests/day02_Locators/FindElement.java
+++ src/test/java/com/cybertek/tests/day02_Locators/FindElement.java
@@ -1,7 +1,24 @@
 package com.cybertek.tests.day02_Locators;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FindElement {
     public static void main(String[] args) {
+        //Locator 1. ID
+        //go to cybertek okta login page
+        //write email to the text box
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://cybertekschool.okta.com/");
 
+        // find the text box first--> findElement()  from WebDriver
+        WebElement emailBox=driver.findElement(By.id("okta-signin-username"));
+        emailBox.sendKeys("halimeecavus@gmail.com");
+        WebElement passwordBox=driver.findElement((By.id("okta-signin-password")));
+        passwordBox.sendKeys(("Hengur52"));
     }
 }
