2dc11b75860f62a5edd0cfe1ff1726f6ee79895e
HCavus <halimeecavus@gmail.com>
2020-04-08 19:37:27 -0400

    add last commit

--- src/test/java/com/cybertek/utilities/WebDriverFactory.java
+++ src/test/java/com/cybertek/utilities/WebDriverFactory.java
@@ -3,36 +3,29 @@ package com.cybertek.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverFactory {
-    /*
-    Write a return method which returns WebDriver
-    method name: getDriver()
-    it takes a String as a parameter--> browser type
-    returns ChromeDriver and FireFoxDriver
-     */
-    public static WebDriver getDriver(String browserType) {
-        //local variables must be initialized
-        WebDriver driver=null;
-    switch(browserType.toLowerCase()){
-        case "chrome":
-            WebDriverManager.chromedriver().setup();
-            driver= new ChromeDriver();
-            break;
-        case "firefox":
-            WebDriverManager.firefoxdriver().setup();
-           driver=new FirefoxDriver();
-           break;
-        case "edge":
-            WebDriverManager.edgedriver().setup();
-            driver=new EdgeDriver();
-            break;
+    //write a static return method which returns WebDriver
+    //name : getDriver
+    //it takes a String as a parameter  --> browser type
+    //returns ChromeDriver and FirefoxDriver
 
+    public static WebDriver getDriver(String browserType){
+        //local veriables
+        WebDriver driver = null;
 
+        switch (browserType.toLowerCase()){
+            case "chrome" :
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+        }
+        return driver;
     }
-    return driver;
 
-    }
+
 }
